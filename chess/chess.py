7b76e176cf30a219ff64059630f31ed03b53daa7
danielgomez3 <danielgomez3@verizon.net>
2021-12-02 14:23:52 -0700

    header file added

---
+++ chess/chess.py
@@ -0,0 +1,8 @@
+"""
+Name: Daniel Gomez
+Description:
+    This is my chess game in which I may use pygame as a library.
+    Made using git verion control, vim, and my custom vim IDE.
+    Thank you for reading :)
+"""
+
